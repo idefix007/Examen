4fbe76d0fe2fa079479c69596ff8c6c057050307
idefix007 <katia@bbcom.be>
2017-11-18 08:57:32 +0100

    intro

---
+++ src/ServletMembre_add.java
@@ -0,0 +1,38 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+
+@WebServlet(name = "Servlet", urlPatterns ={"/add-membre"})
+public class ServletMembre_add extends HttpServlet {
+
+    private ListeMembre listeMembre = new ListeMembre();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Force l'UTF8
+        request.setCharacterEncoding("UTF-8");
+
+        /*
+         * Récupération des données saisies, envoyées en tant que paramètres
+         * à la validation du formulaire
+         */
+        String nom = request.getParameter( "Membre_Nom" );
+        String prenom = request.getParameter( "Membre_Prenom" );
+        String dateNaissance = request.getParameter( "Membre_DateNaissance" );
+        String club = request.getParameter( "FK_Club" );
+
+        //Ajoute à la liste des membres
+        listeMembre.ajouteMembre (new Membre (nom, prenom, dateNaissance, club));
+
+        response.sendRedirect("/membre");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getRequestDispatcher("/vues/membreAdd.jsp").forward(request,response);
+    }
+
+}
---
+++ web/vues/membreAdd.jsp
@@ -0,0 +1,50 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Gestion des membres de clubs</title>
+</head>
+<body>
+
+
+<div class="container">
+    <main class="row justify-content-center">
+
+
+        <div class="col-6">
+
+            <h2> Nouveau membre</h2>
+
+            <!-- Formulaire - Ajouter un nouveau membre -->
+
+            <form action="/add-membre" method="post">
+
+                <div class="form-group">
+                    <input class="form-control" placeholder="Nom du membre"
+                           type="text" name="Nom du membre">
+                </div>
+
+                <div class="form-group">
+                    <input class="form-control" placeholder="Prénom du membre"
+                           type="text" name="Prénom du membre">
+                </div>
+
+                <div class="form-group">
+                    <input class="form-control" placeholder="Date de Naissance"
+                           type="date" name="Date de Naissance">
+                </div>
+
+                <!-- Select option pour liste déroulante -->
+                <div class="form-group">
+                    <select><option></option></select>
+                </div>
+
+                <input type="submit" class="btn btn-default btn-block" name="Ajouter">
+
+            </form>
+        </div> <!--fin du container-->
+    </main>
+
+</div>
+
+</body>
+</html>

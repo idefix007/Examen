053b16422ad11a107958b943514174f271867971
stkdm <stkdm@hotmail.com>
2017-11-20 20:09:21 +0100

    ajout membre

---
+++ src/ServletListeMembre.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ServletListeMembre", urlPatterns = {"/membre"})
+public class ServletListeMembre extends HttpServlet {
+    private ListeMembre listeMembre = new ListeMembre();
+    private ListeClub listeClub = new ListeClub();
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("membres", listeMembre.recupereMembres());
+        request.setAttribute("clubs",listeClub.recupereClubs());
+        request.getRequestDispatcher("/vues/listeMembres.jsp").forward(request,response);
+
+    }
+}
